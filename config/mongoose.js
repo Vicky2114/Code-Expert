2cecfda0f0d9d9333529956b4c391347c171c6ba
Vicky2114 <110174104+Vicky2114@users.noreply.github.com>
2023-01-29 18:00:30 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,9 +1,15 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URL,{
-    useNewUrlParser: true, 
-    useUnifiedTopology: true,
-    family: 4,
-})
-.then(db => console.log('DB is connected'))
-.catch(err => console.log(err));
+mongoose.connect(process.env.MONGODB_URL);
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, "Error connecting to MongoDB"));
+
+
+db.once('open', function(){
+    console.log('Connected to Database :: MongoDB');
+});
+
+
+module.exports = db;
